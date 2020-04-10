a3a93d3267d1be6b0545be17e88e4cdff4180e55
Blake Calvin <blakecalvin@gmail.com>
2020-04-10 00:18:09 -0700

    stop script fix

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-screen -S minecraft -p 0 -X "save-all^M"
-screen -S minecraft -p 0 -X "stop^M"
+screen -S minecraft -p 0 -X stuff "save-all^M"
+screen -S minecraft -p 0 -X stuff "stop^M"
