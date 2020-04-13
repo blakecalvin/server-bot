dd3a857fb3af3527badf48e62476a1d291e00cce
Blake Calvin <blakecalvin@gmail.com>
2020-04-13 01:51:51 -0700

    fix

--- index.js
+++ index.js
@@ -409,8 +409,8 @@ function setWorld(config, name){
 	execSync(`sed -i "s/level-name=maps\\/.*/level-name=maps\\/${version}\\/${name}/" ${config.bot.serverPath}/server.properties`);
 	console.log("after: " + execSync(`cat ${config.bot.serverPath}/server.properties | grep level-name`).toString());
 	console.log("before: " + execSync(`cat ${config.bot.serverPath}/server.properties | grep motd`).toString());
-	execSync(`sed -i "s/${currentWorld}/${name}/" ${config.bot.serverPath}/server.properties`);
-	execSync(`sed -i "s/${currentVersion}/${version}/" ${config.bot.serverPath}/server.properties`);
+	execSync(`sed "s/${currentWorld}/${name}/" ${config.bot.serverPath}/server.properties`);
+	execSync(`sed "s/${currentVersion}/${version}/" ${config.bot.serverPath}/server.properties`);
 	console.log("after: " + execSync(`cat ${config.bot.serverPath}/server.properties | grep motd`).toString());
 }
 
